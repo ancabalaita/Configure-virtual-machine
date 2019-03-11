08c237ccfbe3498a6e33c6a4a904635afba95982
Anca <ancaa55@gmail.com>
2019-03-11 10:57:26 -0400

    merge

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ ! -f config.yaml ]]; then
+if [[ ! -f /root/tema1/config.yaml ]]; then
 	echo "File does not exist" &> /var/log/system-bootstrap.log
 
 else
 
-	myhostname=$(awk -F"=" '{ print $2 }' config.yaml)
+	myhostname=$(awk -F"=" '{ print $2 }'  /root/tema1/config.yaml)
 	hostnamectl set-hostname $myhostname &> /var/log/system-bootstrap.log
 	echo $myhostname &> /var/log/system-bootstrap.log
 	
@@ -13,12 +13,12 @@ else
 	
 	yum check-update &> /var/log/system-bootstrap.log
 	
-	sudo sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config &> /var/log/system-bootstrap.log
+	sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config &> /var/log/system-bootstrap.log
 	setenforce 0 &> /var/log/system-bootstrap.log
 	
 	ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa &> /var/log/system-bootstrap.log
 	ssh-copy-id root@192.168.56.102 &> /var/log/system-bootstrap.log
 	sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config &> /var/log/system-bootstrap.log
-	sudo service sshd restart &> /var/log/system-bootstrap.log
+	service sshd restart &> /var/log/system-bootstrap.log
 
 fi
